b47c408f977d138a8e24ed2de2a7dcaeacb60465
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-09-07 00:01:38 +0200

    login & control page tweaks

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -1,29 +1,29 @@
 function Controls({defaultHidden=false, helpRef}) {
 
     return (
-        <div className="center text-center font-semibold" ref={helpRef} hidden={defaultHidden} tabIndex={0}>
+        <div className="center text-center font-semibold cursor-default" ref={helpRef} hidden={defaultHidden} tabIndex={0}>
             <h2 className="text-3xl mb-6">Controls</h2>
             <ul>
-                <li className="p-1 m-3">
-                    <span className="bg-gray-600 border border-black-10 p-2 m-2 text-xs">Arrows</span>
-                    Change canvas count
+                <li className="m-3">
+                    <div className="bg-gray-600 inline-block border border-black-10 m-2 p-2 align-middle text-xs aspect-video w-14">Arrows</div>
+                    <span className="align-middle">Set number of canvas</span>
                 </li>
-                <li className="p-1 m-3">
-                    <span className="bg-gray-600 border border-black-10 p-2 m-2 text-xs">F</span>
-                    Toggle Fullscreen
+                <li className="m-3">
+                    <div className="bg-gray-600 inline-block border border-black-10 m-2 p-2 align-middle text-xs aspect-square w-8">F</div>
+                    <span className="align-middle">Toggle Fullscreen</span>
                 </li>
-                <li className="p-1 m-3">
-                    <span className="bg-gray-600 border border-black-10 p-2 m-2 text-xs">M</span>
-                    Cycle mirror mode
+                <li className="m-3">
+                    <div className="bg-gray-600 inline-block border border-black-10 m-2 p-2 align-middle text-xs aspect-square w-8">M</div>
+                    <span className="align-middle">Cycle mirror mode</span>
                 </li>
-                <li className="p-1 m-3">
-                    <span className="bg-gray-600 border border-black-10 p-2 m-2 text-xs">L</span>
-                    Log out
+                <li className="m-3">
+                    <div className="bg-gray-600 inline-block border border-black-10 m-2 p-2 align-middle text-xs aspect-square w-8">L</div>
+                    <span className="align-middle">Log out</span>
                 </li>
             </ul>
             <p className="text-md text-gray-400 my-8">
                 You can toggle this popup at any time by pressing
-                <span className="bg-gray-600 border border-black-10 p-1 m-2 text-white text-xs">H</span>
+                <div className="bg-gray-600 inline-block border border-black-10 m-2 p-2 text-white align-middle text-xs aspect-square w-8">H</div>
             </p>
         </div>
     )
